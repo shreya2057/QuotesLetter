90a7333f90aa8a122437af612f4a9ecec61168a1
shreya2057 <shreya2057@gmail.com>
2022-10-17 20:36:46 +0545

    Homescreen with card

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -25,9 +25,66 @@ class _HomeScreenState extends State<HomeScreen> {
             shrinkWrap: true,
             itemCount: 30,
             itemBuilder: (BuildContext context, int index) {
-              // return Text(authorName(1).toString());
-              return Text("hi");
+              return Authors(
+                index: index,
+              );
             }),
+      ),
+    );
+  }
+}
+
+class Authors extends StatefulWidget {
+  const Authors({Key? key, required this.index}) : super(key: key);
+
+  final int index;
+
+  @override
+  State<Authors> createState() => _AuthorsState();
+}
+
+class _AuthorsState extends State<Authors> {
+  List<Color> colors = [
+    const Color.fromARGB(255, 237, 72, 127),
+    const Color.fromARGB(255, 185, 72, 237),
+    const Color.fromARGB(255, 125, 253, 96),
+    const Color.fromARGB(255, 94, 161, 238)
+  ];
+
+  int pickColor(int index) {
+    if (index < colors.length) {
+      int color = index;
+      return color;
+    } else {
+      int color = index % colors.length;
+      return color;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String author = "Shreya";
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+      child: Card(
+        color: const Color.fromARGB(255, 241, 239, 239),
+        elevation: 2,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Icon(
+                Icons.account_circle,
+                size: 45,
+                color: colors[pickColor(widget.index)],
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Text(author),
+            ],
+          ),
+        ),
       ),
     );
   }
